2ff03b22c9b5c9eea8cafb44415b789550e55356
Anton Shelepov <anton.schelepov@gmail.com>
2021-04-01 10:43:34 +0200

    Update etl.py

--- Project_1_DataModelingPostgres/etl.py
+++ Project_1_DataModelingPostgres/etl.py
@@ -111,7 +111,7 @@ def main():
     
     - sends files to ETL
     """
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user= password=")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
