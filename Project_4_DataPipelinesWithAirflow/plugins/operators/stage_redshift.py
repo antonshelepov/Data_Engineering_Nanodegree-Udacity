2f4a5efe9d3c5b6158b347b7bec7032ccd378c89
Anton Shelepov <anton.schelepov@gmail.com>
2021-06-26 16:23:15 +0200

    transfer class added

--- Project_4_DataPipelinesWithAirflow/plugins/operators/stage_redshift.py
+++ Project_4_DataPipelinesWithAirflow/plugins/operators/stage_redshift.py
@@ -1,26 +1,87 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
+    """
+    This method transfers data from S3 to staging tables in redshift database.
+    
+    params:
+    :aws_credentials_id: Conn Id of the Airflow connection to Amazon Web Services
+    :redshift_conn_id: Conn Id of the Airflow connection to redshift database
+    :table: name of the staging table to populate
+    :s3_bucket: name of S3 bucket, e.g. "udacity-dend"
+    :s3_key: name of S3 key. This field is templatable when context is enabled, e.g. "log_data/{execution_date.year}/{execution_date.month}/"
+    :delimiter: csv field delimiter
+    :ignore_headers: '0' or '1'
+    :data_format: 'csv' or 'json'
+    :jsonpaths: path to JSONpaths file
+    
+    returns: None
+    """
+    template_fields = ("s3_key",)
     ui_color = '#358140'
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        {}
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 aws_credentials_id="",
+                 redshift_conn_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 delimiter=",",
+                 ignore_headers=1,
+                 data_format="csv",
+                 jsonpaths="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.aws_credentials_id=aws_credentials_id
+        self.redshift_conn_id=redshift_conn_id
+        self.table=table
+        self.s3_bucket=s3_bucket # udacity-dend
+        self.s3_key=s3_key # log_data, song_data
+        self.delimiter=delimiter
+        self.ignore_headers=ignore_headers
+        self.data_format=data_format.lower()     # 'csv', 'json'
+        self.jsonpaths=jsonpaths
+
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
-
-
-
-
-
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+        
+        self.log.info("Copying data from S3 to Redshift")
+        
+        # csv or json format
+        if self.data_format == 'csv':
+            autoformat = "DELIMITER '{}'".format(self.delimiter)
+        elif self.data_format == 'json':
+            json_option = self.jsonpaths or 'auto'
+            autoformat = "FORMAT AS JSON '{}'".format(json_option)
+        
+        rendered_key = self.s3_key.format(**context)
+        self.log.info('Rendered key is ' + rendered_key)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.ignore_headers,
+            autoformat
+        )
+        redshift.run(formatted_sql)
