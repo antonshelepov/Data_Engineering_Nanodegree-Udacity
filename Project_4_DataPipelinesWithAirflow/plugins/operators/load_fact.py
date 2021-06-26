5e2a67ddffc39b492f13377beda1a606ad954a37
Anton Shelepov <anton.schelepov@gmail.com>
2021-06-26 16:22:27 +0200

    load 2 fact table added

--- Project_4_DataPipelinesWithAirflow/plugins/operators/load_fact.py
+++ Project_4_DataPipelinesWithAirflow/plugins/operators/load_fact.py
@@ -3,20 +3,34 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadFactOperator(BaseOperator):
-
+    """
+    This method loads data into fact table from staging tables.
+    
+    params:
+    :redshift_conn_id: Conn Id of the Airflow connection to redshift database
+    :destination_table: name of the fact table to update
+    :sql_statement: 'select' query to retrieve rows for insertion in fact table
+    
+    returns: None
+    """
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id = "",
+                 destination_table = "",
+                 sql_statement = "",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.destination_table=destination_table
+        self.sql_statement=sql_statement
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info('Fetching redshift hook')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info('Loading fact table')
+        insert_query = 'INSERT INTO {} ({})'.format(self.destination_table, self.sql_statement)
+        redshift.run(insert_query)
