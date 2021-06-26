583e131a8bbc3b2251fce6538c3ef016c4405300
Anton Shelepov <anton.schelepov@gmail.com>
2021-06-26 16:21:56 +0200

    load class added

--- Project_4_DataPipelinesWithAirflow/plugins/operators/load_dimension.py
+++ Project_4_DataPipelinesWithAirflow/plugins/operators/load_dimension.py
@@ -3,20 +3,41 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
+    """
+    This method loads data into dimension table from staging tables.
+   
+    params:
+    :redshift_conn_id: Conn Id of the Airflow connection to redshift database
+    :destination_table: name of the dimension table to update
+    :sql_statement: 'select' query to retrieve rows for insertion in destination table
+    :update_mode: 'insert' or 'overwrite'. 'overwrite' truncates the destination table before inserting rows
+    
+    returns: None
+    """
 
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id = "",
+                 destination_table = "",
+                 sql_statement = "",
+                 update_mode = "overwrite",  # insert, overwrite
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.destination_table=destination_table
+        self.sql_statement=sql_statement
+        self.update_mode=update_mode
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('Fetching redshift hook')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info('Loading dimension table {}'.format(self.destination_table))
+        if self.update_mode == 'overwrite':
+            update_query = 'TRUNCATE {}; INSERT INTO {} ({})'.format(self.destination_table, self.destination_table, self.sql_statement)
+        elif self.update_mode == 'insert':
+            update_query = 'INSERT INTO {} ({})'.format(self.destination_table, self.sql_statement)
+        redshift.run(update_query)
