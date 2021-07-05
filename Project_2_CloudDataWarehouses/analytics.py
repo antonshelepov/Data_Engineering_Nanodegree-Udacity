6f4b185a9230529aa9c260870d2fc88e34d29859
Anton Shelepov <anton.schelepov@gmail.com>
2021-07-05 17:41:51 +0200

    test queries added

---
+++ Project_2_CloudDataWarehouses/analytics.py
@@ -0,0 +1,32 @@
+import configparser
+import psycopg2
+from sql_queries import analytical_queries, analytical_query_titles
+
+
+def run_analytical_queries(cur):
+    """This function runs all analytics queries and returns number of row.total
+    
+    Params: 
+        cur:
+        
+    Returns:
+    """
+    for query in analytical_queries:
+        row = cur.execute(query)
+        print(f"For query {query} number of rows returned: {row.total}")
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+
+    run_analytical_queries(cur)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
